9b5c038045ae0c30b5a59d40fe1f321a2280f0dd
Sander Geenen <sander.geenen@studiohyperdrive.be>
2024-12-05 11:11:45 +0100

    chore(internal-plugin): add scope crawler logic

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -1,12 +1,27 @@
 import {
-  addProjectConfiguration,
   formatFiles,
-  generateFiles,
+  ProjectConfiguration,
   Tree,
+  updateJson,
 } from '@nx/devkit';
-import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
 import { libraryGenerator } from '@nx/js';
+
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const allScopes: string[] = Array.from(projectMap.values())
+    .map((project) => {
+      if (project.tags) {
+        const scopes = project.tags.filter((tag: string) => tag.startsWith('scope:'));
+        return scopes;
+      }
+      return [];
+    })
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+
+  // remove duplicates
+  return Array.from(new Set(allScopes));
+}
 
 export async function utilLibGenerator(
   tree: Tree,
@@ -16,6 +31,18 @@ export async function utilLibGenerator(
     directory: `libs/${options.directory}/${options.name}`,
   });
   await formatFiles(tree);
+
+  updateJson(tree, '/libs/internal-plugin/src/generators/util-lib/schema.json.json', (schemaJson) => {
+    const scopes = getScopes(schemaJson.projectMap);
+
+    schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
+      value: scope,
+      label: scope,
+    }));
+    schemaJson.properties.directory.enums = scopes;
+
+    return schemaJson;
+  });
 }
 
 export default utilLibGenerator;
