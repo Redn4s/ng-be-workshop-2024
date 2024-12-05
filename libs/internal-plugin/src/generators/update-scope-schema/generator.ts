99dde894afd7e9ec029e939fbc2a4daa2121ce62
Sander Geenen <sander.geenen@studiohyperdrive.be>
2024-12-05 11:28:14 +0100

    feat(internal-plugin/update-scope-schema): update schema interface logic to generator

--- libs/internal-plugin/src/generators/update-scope-schema/generator.ts
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -1,44 +1,48 @@
 import {
   formatFiles,
+  getProjects,
   ProjectConfiguration,
   Tree,
   updateJson,
 } from '@nx/devkit';
 
 function getScopes(projectMap: Map<string, ProjectConfiguration>) {
-  const allScopes: string[] = Array.from(projectMap.values())
-    .map((project) => {
-      if (project.tags) {
-        const scopes = project.tags.filter((tag: string) => tag.startsWith('scope:'));
-        return scopes;
-      }
-      return [];
-    })
+  const projects: any[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) => project.tags.filter((tag: string) => tag.startsWith('scope:')))
     .reduce((acc, tags) => [...acc, ...tags], [])
     .map((scope: string) => scope.slice(6));
 
-  // remove duplicates
   return Array.from(new Set(allScopes));
 }
 
-export async function updateScopeSchemaGenerator(
-  tree: Tree,
-) {
-  updateJson(tree, 'nx.json', (json) => ({
-    ...json,
-    defaultProject: 'movies-app',
-  }));
-  updateJson(tree, '/libs/internal-plugin/src/generators/update-scope-schema/schema.json', (schemaJson) => {
-    const scopes = getScopes(schemaJson.projectMap);
-
+function updateSchemaJson(tree: Tree, scopes: string[]) {
+  updateJson(tree, 'libs/internal-plugin/src/generators/update-scope-schema/schema.json', (schemaJson) => {
     schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
       value: scope,
       label: scope,
     }));
     schemaJson.properties.directory.enums = scopes;
-
     return schemaJson;
   });
+}
+
+function updateSchemaInterface(tree: Tree, scopes: string[]) {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const interfaceDefinitionFilePath = 'libs/internal-plugin/src/generators/update-scope-schema/schema.d.ts';
+  const newContent = `export interface UpdateScopeSchemaGeneratorSchema {
+    name: string;
+    directory: ${joinScopes};
+  }`;
+  tree.write(interfaceDefinitionFilePath, newContent);
+}
+
+export async function updateScopeSchemaGenerator(
+  tree: Tree,
+) {
+  const scopes = getScopes(getProjects(tree));
+  updateSchemaJson(tree, scopes);
+  updateSchemaInterface(tree, scopes);
   await formatFiles(tree);
 }
 
